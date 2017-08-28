a52a82f96ea4c35f3c62fe2d1b83fb5a97d21f8e
Matt Menzenski <matt@mobilgov.com>
2017-08-27 21:11:13 -0500

    add version option to cli

--- tokened/cli/__init__.py
+++ tokened/cli/__init__.py
@@ -1,10 +1,13 @@
 import click
 
+from ..__about__ import __version__
+
 from ..config import configure_logging
 
 
 @click.command()
 @click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
+@click.version_option(version=__version__)
 @click.argument('name', default='world', required=False)
 def main(name, as_cowboy):
     """Tohono O'odham text parsing and analysis."""
